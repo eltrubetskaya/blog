de7128afa387d532395f3e503a1bcf0e0b05ce3a
el.trubetskaya <el.trubetskaya@gmail.com>
2016-12-30 15:18:25 +0200

    describe entity

---
+++ src/Veta/HomeworkBundle/Entity/Tag.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Veta\HomeworkBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as SymfonyConstraints;
+use Veta\HomeworkBundle\Entity\Post;
+
+/**
+ * Tag
+ *
+ * @ORM\Entity(repositoryClass="Veta\HomeworkBundle\Repository\TagRepository")
+ * @UniqueEntity("title")
+ * @UniqueEntity("slug")
+ */
+class Tag
+{
+    /**
+     * @var integer
+     * @ORM\Column(name="id", type="integer")
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(name="title", type="string", length=255, unique=true)
+     *
+     * @SymfonyConstraints\NotBlank()
+     * @SymfonyConstraints\Length(
+     *     min="3"
+     * )
+     * @SymfonyConstraints\Type(
+     *     type="string"
+     * )
+     */
+    private $title;
+
+    /**
+     * @var string
+     * @ORM\Column(name="slug", type="string", length=255, unique=true, nullable=true)
+     *
+     * @Gedmo\Slug(fields={"title"})
+     */
+    private $slug;
+
+    /**
+     * @var Collection
+     *
+     * @ORM\ManyToMany(targetEntity="Post", mappedBy="tags", cascade={"persist","detach","merge"})
+     */
+    private $posts;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Tag
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Add post
+     *
+     * @param Post $post
+     *
+     * @return Tag
+     */
+    public function addPost(Post $post)
+    {
+        $this->posts[] = $post;
+
+        return $this;
+    }
+
+    /**
+     * Remove post
+     *
+     * @param Post $post
+     */
+    public function removePost(Post $post)
+    {
+        $this->posts->removeElement($post);
+    }
+
+    /**
+     * Get posts
+     *
+     * @return Collection
+     */
+    public function getPosts()
+    {
+        return $this->posts;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getTitle();
+    }
+}
